3976170214db1f8ba45536d6b8c904669e365fda
Tamas Takacs <takacs.tamas.1@gmail.com>
2014-06-17 23:14:30 +0200

    Final result of yeoman tutorial.
    see: http://yeoman.io/codelab.html

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope, localStorageService) {
+    var todosInStore = localStorageService.get('todos');
+
+	$scope.todos = todosInStore && todosInStore.split('\n') || [];
+
+	$scope.$watch('todos', function () {
+	  localStorageService.add('todos', $scope.todos.join('\n'));
+	}, true);
+
+	$scope.addTodo = function () {
+      if ($scope.todos.indexOf($scope.todo) === -1) {   		
+		  $scope.todos.push($scope.todo);
+		  $scope.todo = '';
+		}
+	};
+	$scope.removeTodo = function (index) {
+	  $scope.todos.splice(index, 1);
+	};
+  });
